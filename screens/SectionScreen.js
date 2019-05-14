c3736492d7e377f096686bdfeeda940d0d467e93
Patrick Dugan <dugan.pat@gmail.com>
2019-05-13 17:22:23 -0700

    HTML markdown

--- screens/SectionScreen.js
+++ screens/SectionScreen.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Button, TouchableOpacity, StatusBar } from 'react-native';
+import Markdown from "react-native-showdown";
 import { Icon } from 'expo';
+import {
+    Button,
+    TouchableOpacity,
+    StatusBar,
+    WebView,
+    Linking,
+    ScrollView,
+} from 'react-native';
 
 class SectionScreen extends React.Component {
     static navigationOptions = {
@@ -22,37 +30,125 @@ class SectionScreen extends React.Component {
         const section = navigation.getParam('section');
 
         return (
-            <Container>
-                <StatusBar hidden />
-                <Cover>
-                    <Image source={{ uri: section.image.url }} />
-                    <Wrapper>
-                        <Logo source={{ uri: section.logo.url }} />
-                        <Subtitle>{section.subtitle}</Subtitle>
-                    </Wrapper>
-                    <Title>{section.title}</Title>
-                    <Caption>{section.caption}</Caption>
-                </Cover>
-                <TouchableOpacity
-                    onPress={() => {
-                        this.props.navigation.goBack();
-                    }}
-                    style={{ position: 'absolute', top: 20, right: 20 }}>
-                    <CloseView>
-                        <Icon.Ionicons
-                            name='ios-close'
-                            size={36}
-                            color='#4775f2'
-                            style={{ marginTop: -2 }}
+            <ScrollView>
+                <Container>
+                    <StatusBar hidden />
+                    <Cover>
+                        <Image source={{ uri: section.image.url }} />
+                        <Wrapper>
+                            <Logo source={{ uri: section.logo.url }} />
+                            <Subtitle>{section.subtitle}</Subtitle>
+                        </Wrapper>
+                        <Title>{section.title}</Title>
+                        <Caption>{section.caption}</Caption>
+                    </Cover>
+                    <TouchableOpacity
+                        onPress={() => {
+                            this.props.navigation.goBack();
+                        }}
+                        style={{ position: 'absolute', top: 20, right: 20 }}>
+                        <CloseView>
+                            <Icon.Ionicons
+                                name='ios-close'
+                                size={36}
+                                color='#4775f2'
+                                style={{ marginTop: -2 }}
+                            />
+                        </CloseView>
+                    </TouchableOpacity>
+                    <Content>
+                         {/* <WebView
+                            source={{ html: htmlContent + htmlStyles }}
+                            scalesPageToFit={false}
+                            scrollEnabled={false}
+                            ref='webview'
+                            onNavigationStateChange={event => {
+                                if (event.url != 'about:blank') {
+                                    this.refs.webview.stopLoading();
+                                    Linking.openURL(event.url);
+                                }
+                            }}
+                        /> */}
+                        <Markdown
+                            body={section.content}
+                            pureCSS={htmlStyles}
+                            scalesPageToFit={false}
+                            scrollEnabled={false}
                         />
-                    </CloseView>
-                </TouchableOpacity>
-            </Container>
+                    </Content>
+                </Container>
+            </ScrollView>
         );
     }
 }
 
 export default SectionScreen;
+
+const htmlContent = `
+    <h2>This is a title</h2>
+    <p>This <strong>is</strong> a <a href='http://designcode.io'>link</a></p>
+    <img src='https://cl.ly/8861f359ed6d/download/Wave14.jpg' />
+`;
+
+const htmlStyles = `
+<style>
+    * {
+        font-family: -apple-system;
+        margin: 0;
+        padding: 0;
+        font-size: 17px;
+        font-weight: normal;
+        color: #3c4560;
+        line-height: 24px;
+    }
+
+    h2 {
+        font-size: 20px;
+        text-transform: uppercase;
+        color: #b8bece;
+        font-weight: 600;
+        margin-top: 50px;
+    }
+
+    p {
+        margin-top: 20px;
+    }
+
+    a {
+        color: #4775f2;
+        font-weight: 600;
+        text-decoration: none;
+    }
+
+    strong {
+        font-weight: 700;
+    }
+
+    img {
+        width: 100%;
+        margin-top: 20px;
+        border-radius: 10px;
+    }
+    pre {
+        padding: 20px;
+        background: #212C4F;
+        overflow: hidden;
+        word-wrap: break-word;
+        border-radius: 10px;
+        margin-top: 20px;
+    }
+
+    code {
+        color: white;
+    }
+</style>
+`;
+
+
+const Content = styled.View`
+    height: 1000px;
+    padding: 20px;
+`;
 
 const Container = styled.View`
     flex: 1;
